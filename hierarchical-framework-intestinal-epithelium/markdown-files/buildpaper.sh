3d0f637fe13433aa67361fbb86e6acc13673bbef
omaclaren <o.maclaren@gmail.com>
2016-10-25 13:24:35 +1300

    add link to github page

--- hierarchical-framework-intestinal-epithelium/markdown-files/buildpaper.sh
+++ hierarchical-framework-intestinal-epithelium/markdown-files/buildpaper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#NOTE: if you want to use this build script you will need to change --data-dir=$HOME/Git-Working/hierarchical-framework-intestinal-epithelium/markdown-files to point the appropriate location you clone the repo to.
+#NOTE: if you want to use this build script you will need to change --data-dir=$HOME/Git-Working/hierarchical-framework-intestinal-epithelium/markdown-files to point the appropriate location you clone the repo to. You also need to make sure the pandoc filters used below are installed.
 
 pandoc --filter pandoc-fignos --filter pandoc-eqnos --filter pandoc-citeproc --data-dir=$HOME/Git-Working/hierarchical-framework-intestinal-epithelium/markdown-files --template plos2015.latex --latex-engine=xelatex hierarchical-intestinal.md -o ../manuscript-pdf/hierarchical-intestinal.pdf
+
+pandoc supplementary-information.md -o ../manuscript-pdf/supplementary-information.pdf
 
 #
 #--standalone --natbib --bibliography=../bibtex-files/crypt-villus-refs.bib
---
+++ hierarchical-framework-intestinal-epithelium/markdown-files/supplementary-information.md
@@ -0,0 +1,4 @@
+#Supplementary information
+All manuscript, code and data files can be found at the following github page: 
+
+<span style="color:blue">[https://github.com/omaclaren/open-manuscripts/tree/master/hierarchical-framework-intestinal-epithelium](https://github.com/omaclaren/open-manuscripts/tree/master/hierarchical-framework-intestinal-epithelium)</span>
