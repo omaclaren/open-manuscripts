3f3359e2801ee78ee378b69938f6d810c8928348
omaclaren <o.maclaren@gmail.com>
2017-03-08 20:24:04 +1300

    included a pdf version without a logo

--- hierarchical-framework-intestinal-epithelium/markdown-files/buildpaper.sh
+++ hierarchical-framework-intestinal-epithelium/markdown-files/buildpaper.sh
@@ -3,6 +3,8 @@
 
 pandoc --filter pandoc-fignos --filter pandoc-eqnos --filter pandoc-citeproc --data-dir=$HOME/Git-Working/hierarchical-framework-intestinal-epithelium/markdown-files --template plos2015.latex --latex-engine=xelatex hierarchical-intestinal.md -o ../manuscript-pdf/hierarchical-intestinal.pdf
 
+pandoc --filter pandoc-fignos --filter pandoc-eqnos --filter pandoc-citeproc --data-dir=$HOME/Git-Working/hierarchical-framework-intestinal-epithelium/markdown-files --template plos2015_nologo.latex --latex-engine=xelatex hierarchical-intestinal.md -o ../manuscript-pdf/hierarchical-intestinal-nologo.pdf
+
 pandoc --filter pandoc-fignos --filter pandoc-eqnos --filter pandoc-citeproc --data-dir=$HOME/Git-Working/manuscripts-and-notes/crypt-villus-hierarchical-paper/markdown-files --latex-engine=xelatex supplementary-information.md -o ../manuscript-pdf/supplementary-information.pdf
 
 #
